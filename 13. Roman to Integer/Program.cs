5ca1c90d68f22c9eb03a01029f0f61fd03728844
Zetka <jakub.kochanski10@gmail.com>
2022-07-11 22:51:09 +0200

    Dodaj pliki projektów.

---
+++ 13. Roman to Integer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RomanToInt("III"));
+            Console.WriteLine(RomanToInt("LVIII"));
+            Console.WriteLine(RomanToInt("MCMXCIV"));
+        }
+        static Dictionary<char, int> dict = new Dictionary<char, int>()
+        {
+            {'I',1},
+            {'V',5},
+            {'X',10},
+            {'L',50},
+            {'C',100},
+            {'D',500},
+            {'M',1000}
+        };
+        public static int RomanToInt(string s)
+        {
+            int result = 0;
+            int maxValue = 1;
+            for (int i = s.Length-1; i>=0 ; i--)
+            {
+                if(maxValue <= dict[s[i]])
+                {
+                    if (maxValue < dict[s[i]])
+                    {
+                        maxValue = dict[s[i]];
+                    }
+                    result += maxValue;
+                }
+                else
+                {
+                    result -= dict[s[i]];
+                }
+            }
+            return result;
+        }
+
+    }
+}
