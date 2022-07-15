66f04770c59cf1158c81a00a0c13735444815e8f
Zetka <jakub.kochanski10@gmail.com>
2022-07-15 21:05:30 +0200

    206. Reverser Linked List

---
+++ 206. Reverse Linked List/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MyApp
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode left = null;
+            ListNode temp;
+            while(head != null)
+            {
+                temp = head.next;
+                head.next = left;
+                left = head;
+                head = temp;
+            }
+            return left;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode head = new ListNode(1);
+            head.next = new ListNode(2);
+            head.next.next = new ListNode(3);
+            Solution solution = new Solution();
+            ListNode reversed = solution.ReverseList(head);
+            while (reversed != null)
+            {
+                Console.WriteLine(reversed.val);
+                reversed = reversed.next;
+            }
+        }
+    }
+}
