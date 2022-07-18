26f180975433dd51f9216d41998d3f219f16a96f
Zetka <jakub.kochanski10@gmail.com>
2022-07-18 09:18:24 +0200

    412. Fizz Buzz

---
+++ 412. Fizz Buzz/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace MyApp 
+{
+    public class Solution
+    {
+        public IList<string> FizzBuzz(int n)
+        {
+            IList<string> list = new List<string>();
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 15 == 0)
+                {
+                    list.Add("FizzBuzz");
+                }
+                else if(i % 5 == 0)
+                {
+                    list.Add("Buzz");
+                }
+                else if(i % 3 == 0)
+                {
+                    list.Add("Fizz");
+                }
+                else
+                {
+                    list.Add(Convert.ToString(i));
+                }
+            }
+            return list;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
