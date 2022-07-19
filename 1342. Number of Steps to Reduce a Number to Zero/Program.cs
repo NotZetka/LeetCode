ad4856fb8adf464aa2951567b94da922693fd86e
Zetka <jakub.kochanski10@gmail.com>
2022-07-19 17:12:50 +0200

    1342. Number of steps to reduce

---
+++ 1342. Number of Steps to Reduce a Number to Zero/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MyApp 
+{
+    public class Solution
+    {
+        public int NumberOfSteps2(int num)
+        {
+            return Helper(num, 0);
+        }
+        public int Helper(int num, int steps)
+        {
+            if (num == 0) return steps;
+            if (num % 2 == 0) return Helper(num / 2, steps+1);
+            else return Helper(num-1 , steps+1);
+        }
+        public int NumberOfSteps(int num)
+        {
+            int counter = 0;
+            while (num != 0)
+            {
+                if (num % 2 == 0)
+                {
+                    num = num/2;
+                }
+                else
+                {
+                    num--;
+                }
+                counter++;
+            }
+            return counter;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
