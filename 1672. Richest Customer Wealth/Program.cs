3a4a68f65a07fa20b4127fec0d1e3be307896fe7
Zetka <jakub.kochanski10@gmail.com>
2022-07-20 09:29:48 +0200

    1672. Richest Customer Wealth

---
+++ 1672. Richest Customer Wealth/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApp 
+{
+    public class Solution
+    {
+        public int MaximumWealth(int[][] accounts)
+        {
+            int max = 0;
+            foreach (var customer in accounts)
+            {
+                int balance = customer.Sum();
+                if(balance > max) { max = balance; };
+            }
+            return max;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
