0481d77f6bd6d2cf337a40e6d28f8dfb1115fab2
Zetka <jakub.kochanski10@gmail.com>
2022-07-20 09:36:35 +0200

    1480. Running Sum of 1d Array

---
+++ 1480. Running Sum of 1d Array/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyApp 
+{
+    public class Solution
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i++)
+            {
+                nums[i] = nums[i] + nums[i - 1];
+            }
+            return nums;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
