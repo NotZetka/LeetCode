d0a296281a9254be683d65dbcfc32931bd047314
Zetka <jakub.kochanski10@gmail.com>
2022-07-20 10:32:14 +0200

    724. Find Pivot Index

---
+++ 724. Find Pivot Index/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyApp 
+{
+    public class Solution
+    {
+        public int PivotIndex(int[] nums)
+        {
+            int right = nums.Sum();
+            int left = 0;
+            int current;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                current = nums[i];
+                right-=current;
+                if (left == right) { return i; }
+                left+=current;
+            }
+            return -1;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
