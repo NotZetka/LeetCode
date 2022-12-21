f2047d75871e7f244c6bda5234fa7131681ef1d0
Zetka <jakub.kochanski10@gmail.com>
2022-12-21 18:51:33 +0100

    33._Search_in_Rotated_Sorted_Array 32._Longest_Valid_Parentheses

---
+++ 32. Longest Valid Parentheses/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _32._Longest_Valid_Parentheses
+{
+    //public class Solution
+    //{
+    //    public int LongestValidParentheses(string s)
+    //    {
+    //        int start = 0;
+    //        while (start < s.Length && s[start] != '(')
+    //        {
+    //            start++;
+    //        }
+    //        return helper(s,start);
+    //    } 
+    //    private int helper(string s, int start)
+    //    {
+    //        if(start >= s.Length) return 0;
+    //        if(s[start] == ')') return helper(s,start+1);
+    //        int openCounter = 1;
+    //        int closeCounter = 0;
+    //        int end = start + 1;
+    //        int result = 0;
+    //        while (end < s.Length && openCounter>=closeCounter)
+    //        {
+    //            if (s[end] == '(')
+    //            {
+    //                openCounter++;
+    //            }
+    //            else
+    //            {
+    //                closeCounter++;
+    //            }
+    //            end++;
+    //            if(openCounter == closeCounter)
+    //            {
+    //                result = end - start;
+    //            }
+    //        }
+    //        return Math.Max(result, helper(s, start + 1));
+    //    }
+    //}
+    public class Solution
+    {
+        public int LongestValidParentheses(string s)
+        {
+            int result = 0;
+            int openCounter = 0;
+            int closeCounter = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    openCounter++;
+                }
+                else
+                {
+                    closeCounter++;
+                }
+                if(openCounter == closeCounter)
+                {
+                    result = Math.Max(result, openCounter+closeCounter);
+                }else if(closeCounter > openCounter)
+                {
+                    openCounter = 0;
+                    closeCounter = 0;
+                }
+            }
+            openCounter = 0;
+            closeCounter = 0;
+            for (int i = s.Length-1; i >= 0 ; i--)
+            {
+                if (s[i] == '(')
+                {
+                    openCounter++;
+                }
+                else
+                {
+                    closeCounter++;
+                }
+                if(openCounter == closeCounter)
+                {
+                    result = Math.Max(result, openCounter+closeCounter);
+                }else if(closeCounter < openCounter)
+                {
+                    openCounter = 0;
+                    closeCounter = 0;
+                }
+            }
+            return result;
+        } 
+        
+    }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Solution solution = new Solution();
+            Console.WriteLine(solution.LongestValidParentheses("(()"));
+            Console.WriteLine(solution.LongestValidParentheses(")()())"));
+            Console.WriteLine(solution.LongestValidParentheses(")"));
+            Console.WriteLine(solution.LongestValidParentheses("()(()"));
+            Console.WriteLine(solution.LongestValidParentheses("))))((()(("));
+            Console.WriteLine(solution.LongestValidParentheses(")()())()()("));
+            Console.WriteLine(solution.LongestValidParentheses("))))((()(("));//2
+            Console.WriteLine(solution.LongestValidParentheses(")(())))(())())"));//6
+            Console.WriteLine(solution.LongestValidParentheses("()(()"));//2
+            }
+        }
+    }
+
---
+++ 33. Search in Rotated Sorted Array/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _33._Search_in_Rotated_Sorted_Array
+{
+    public class Solution
+    {
+        public int Search(int[] nums, int target)
+        {
+            if (nums.Length == 0) return -1;
+            int left = 0;
+            int right = nums.Length - 1;
+            while(left < right)
+            {
+                int mid = (left + right) / 2;
+                if (nums[mid] >= nums[left] && target<= nums[mid] && target >= nums[left])
+                {
+                    right = mid;
+                }else if(nums[mid+1] <= nums[right] && target >= nums[mid + 1] && target <= nums[right])
+                {
+                    left = mid + 1;
+                }else if(nums[mid] < nums[left])
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid+1;
+                }
+            }
+            if (nums[left] == target)
+            {
+                return left;
+            }
+            return -1;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.Search(new int[] { 4, 5, 6, 7, 0, 1, 2 },0));
+            Console.WriteLine(solution.Search(new int[] { 4, 5, 6, 7, 0, 1, 2 },3));
+            Console.WriteLine(solution.Search(new int[] { 1 },0));
+        }
+    }
+}
