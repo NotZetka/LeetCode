40c8789ffbcc9bb58b9659d851e1aca65bac1d8f
Zetka <jakub.kochanski10@gmail.com>
2022-12-27 20:38:19 +0100

    38. Count and Say

---
+++ 38. Count and Say/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+
+namespace _38._Count_and_Say
+{
+    public class Solution
+    {
+        public string CountAndSay(int n)
+        {
+            string result = "1";
+            for (int i = 1; i < n; i++)
+            {
+                result = helper(result);
+            }
+            return result;
+        }
+        private string helper(string s)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            char c = s[0];
+            int counter = 1;
+            for (int i = 1; i < s.Length; i++)
+            {
+                if(s[i] == c)
+                {
+                    counter++;
+                }
+                else
+                {
+                    stringBuilder.Append($"{counter}");
+                    stringBuilder.Append(c);
+                    c = s[i];
+                    counter = 1;
+                }
+            }
+            stringBuilder.Append($"{counter}");
+            stringBuilder.Append(c);
+            return stringBuilder.ToString();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.CountAndSay(1));
+            Console.WriteLine(solution.CountAndSay(2));
+            Console.WriteLine(solution.CountAndSay(3));
+            Console.WriteLine(solution.CountAndSay(4));
+        }
+    }
+}
