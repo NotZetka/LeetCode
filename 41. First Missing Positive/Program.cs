aff46baf21870cd666cff7234d2ca3efec69a1b1
Zetka <jakub.kochanski10@gmail.com>
2022-12-28 12:04:44 +0100

    41. First Missing Positive

---
+++ 41. First Missing Positive/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _41._First_Missing_Positive
+{
+    //public class Solution
+    //{
+    //    public int FirstMissingPositive(int[] nums)
+    //    {
+    //        int result = 1;
+    //        Array.Sort(nums);
+    //        foreach (var item in nums)
+    //        {
+    //            if (item == result)
+    //            {
+    //                result++;
+    //            }
+    //        }
+    //        return result;
+    //    }
+    //}
+    public class Solution
+    {
+        public int FirstMissingPositive(int[] nums)
+        {
+            int l = nums.Length;
+            Dictionary<int, bool> dict = new Dictionary<int, bool>();
+            for (int i = 1; i <= l; i++)
+            {
+                dict[i] = false;
+            }
+            foreach (var item in nums)
+            {
+                if(item <= l && item>0)
+                {
+                    dict[item] = true;
+                }
+            }
+            foreach(var item in dict)
+            {
+                if(!item.Value) return item.Key;
+            }   
+            return l+1;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
