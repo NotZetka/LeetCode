c13be47376f959753b35a337d43bcb6ee4e9e906
Zetka <jakub.kochanski10@gmail.com>
2024-02-23 13:09:25 +0100

    49._Group_Anagrams

---
+++ 49. Group Anagrams/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _49._Group_Anagrams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            var strs = new string[] { "eat", "tea", "tan", "ate", "nat", "bat" };
+            var result = solution.GroupAnagrams(strs);
+            Console.WriteLine();
+        }
+    }
+    public class Solution
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var dict = new Dictionary<string, IList<string>>();
+            foreach (var item in strs)
+            {
+                var anagram = String.Concat(item.OrderBy(x => x));
+                if (dict.ContainsKey(anagram))
+                {
+                    dict[anagram].Add(item);
+                }
+                else
+                {
+                    dict[anagram] = new List<string> { item };
+                }
+            }
+            return dict.Values.ToList();
+        }
+    }
+}
